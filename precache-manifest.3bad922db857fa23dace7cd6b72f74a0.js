482bf82438a1c3184435ee9d8aac2691e33b544c
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-10-22 09:36:14 -0300

    Project Ok!

---
+++ precache-manifest.3bad922db857fa23dace7cd6b72f74a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ccd5a63edf55535fddd09efcb42381",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/index.html"
+  },
+  {
+    "revision": "d9d909659cbf90ea7d8e",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/static/css/main.777d9dc1.chunk.css"
+  },
+  {
+    "revision": "82bcea00dc9c3bf79a37",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/static/js/2.af559d44.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/static/js/2.af559d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d909659cbf90ea7d8e",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/static/js/main.5c66716e.chunk.js"
+  },
+  {
+    "revision": "13d371e003cfea4d44b7",
+    "url": "https://hoyosjuan16.github.io/search-movies-view/static/js/runtime-main.2ad1292a.js"
+  }
+]);
